a74af249860a870b004d5a86672634dcff38e5d7
hideonhp <hideonhp@gmail.com>
2026-08-25 00:39:09 +0700

    chore: remove garbage files

--- fix_test3.py
+++
@@ -1,6 +0,0 @@
-import re
-
-with open("vnikey-ibus/src/main.rs", "r") as f:
-    content = f.read()
-
-# Let's inspect the `test_flow_go_tieng_viet_co_ban` test logic
--- get_issue.py
+++
@@ -1,7 +0,0 @@
-# The issue is probably `keyval_to_char` is returning `None` for these basic keys. Let's check `keyval_to_char`!
-with open("vnikey-ibus/src/main.rs", "r") as f:
-    lines = f.readlines()
-    for i, l in enumerate(lines):
-        if "fn keyval_to_char" in l:
-            print("".join(lines[i:i+10]))
-            break
